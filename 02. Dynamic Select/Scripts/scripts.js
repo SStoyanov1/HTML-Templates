0aafca17f7cff23bca324c74190787fe4a157e2b
SStoyanov1 <hehormajla@gmail.com>
2014-06-14 16:18:56 +0300

    Commit

---
+++ 02. Dynamic Select/Scripts/scripts.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    var items = [{
+        value: 1,
+        text: 'One'
+    }, {
+        value: 2,
+        text: 'Two'
+    }, {
+        value: 3,
+        text: 'Three'
+    }, {
+        value: 4,
+        text: 'Four'
+    }, {
+        value: 5,
+        text: 'Five'
+    }, {
+        value: 6,
+        text: 'Six'
+    }, {
+        value: 7,
+        text: 'Seven'
+    }];
+    
+    var selectTemplateString = document.getElementById('select-template').innerHTML;
+    var selectTemplate = Handlebars.compile(selectTemplateString);
+
+    var selectHTML = selectTemplate({ items: items });
+
+    document.getElementById('wrapper').innerHTML = selectHTML;
+};
---
+++ 03. jQuery Plugin for ListView/Scripts/script.js
@@ -0,0 +1,23 @@
+﻿var gosho = [
+    {
+        number: 1,
+        name: 'Peter Petrov',
+        mark: 5.5
+    },
+    {
+        number: 2,
+        name: 'Stamat Georgiev',
+        mark: 4.7
+    },
+    {
+        number: 3,
+        name: 'Maria Todorova',
+        mark: 6
+    },
+    {
+        number: 4,
+        name: 'Georgi Geshov',
+        mark: 3.7
+    }, ];
+
+$('#students-table').listview(gosho);
